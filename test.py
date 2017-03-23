34c6fe16828eae3d1a9a080d936567f0cf3632f1
Eric Bigelow <eric.j.bigelow@gmail.com>
2017-03-23 11:27:06 -0400

    Initial commit for Tucker Tensor.

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import numpy as np 
+import numpy as np
 from scipy.io.matlab import loadmat
 import tensorflow as tf
 import cp
@@ -6,7 +6,7 @@ import cp
 mat = loadmat('data/bread/brod.mat')
 X = mat['X'].reshape([10,11,8])
 
-T = cp.KruskalTensor(X.shape, rank=3)
-X_predict = T.cp_als(X, tf.train.GradientDescentOptimizer(0.1), epochs=1000)
+T = cp.KruskalTensor(X.shape, rank=3, regularize=1e-4)
+X_predict = T.cp_als(X, tf.train.GradientDescentOptimizer(0.01), epochs=3000)
 
 np.save('X_predict.npy', X_predict)
