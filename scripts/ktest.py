919147f04bbe68f5c19d31fcf60833d1096de5ad
Eric Bigelow <eric.j.bigelow@gmail.com>
2017-04-06 13:35:26 -0400

    Created scripts dir.

--- scripts/ktest.py
+++ scripts/ktest.py
@@ -1,9 +1,11 @@
 import numpy as np
 from scipy.io.matlab import loadmat
 import tensorflow as tf
+
+import sys; sys.path.append('..')
 from ktensor import KruskalTensor
 
-mat = loadmat('data/bread/brod.mat')
+mat = loadmat('../data/bread/brod.mat')
 X = mat['X'].reshape([10,11,8])
 
 # T = KruskalTensor(X.shape, rank=3, regularize=0.0, init='nvecs', X_data=X)
@@ -12,4 +14,4 @@ T = KruskalTensor(X.shape, rank=3, regularize=1e-6, init='nvecs', X_data=X)
 X_predict = T.train_als(X, tf.train.AdadeltaOptimizer(0.05), epochs=30000)
 
 
-np.save('X_predict.npy', X_predict)
+np.save('../data/bread/X_cp.npy', X_predict)
--- scripts/ttest.py
+++ scripts/ttest.py
@@ -1,9 +1,11 @@
 import numpy as np
 import tensorflow as tf
 from scipy.io.matlab import loadmat
+
+import sys; sys.path.append('..')
 from ttensor import TuckerTensor
 
-mat = loadmat('data/bread/brod.mat')
+mat = loadmat('../data/bread/brod.mat')
 X = mat['X'].reshape([10,11,8])
 
 T = TuckerTensor(X.shape, ranks=[3,3,3], regularize=0.0, init='random')
@@ -12,4 +14,4 @@ T = TuckerTensor(X.shape, ranks=[3,3,3], regularize=0.0, init='random')
 X_predict = T.hooi(X, epochs=100)
 #X_predict = T.hosvd(X)
 
-np.save('X_tucker.npy', X_predict)
+np.save('../data/bread/X_tucker.npy', X_predict)
